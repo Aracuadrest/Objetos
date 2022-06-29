14bb12cc01a078af7c684eca5a95cde0e07d4aa9
Araceli <Araceli@DESKTOP-BBV7U01>
2022-06-29 14:22:53 +0200

    Commit inicial con Coche y PruebaCoche

---
+++ Objetos/src/objetos/Coche.java
@@ -0,0 +1,74 @@
+
+package objetos;
+
+	//Nombre de la clase
+public class Coche {
+	
+	// Estado: Variables miembro o variables de instancia
+	private String modelo;
+	private String marca;
+	private int potencia;
+	private int velocidad;
+	private int velocidadMax;
+	
+	
+	//generar constructor por cada objeto por el numero de parametros
+	// constructor vacio(por si hemos creado uno sin variables iniciales)
+	
+	public Coche() {
+		velocidad=0;
+	}
+	
+	//constructor
+	public Coche(String m, String ma, int p, int vm) {
+		modelo=m;
+		marca=ma;
+		potencia=p;
+		velocidadMax=vm;
+	}
+	
+	// Metodos de obtencion y establecimiento
+	
+	public void acelerar() {
+		if (velocidad+10<=velocidadMax) {
+			velocidad=velocidad+10;
+		}
+	}
+	public void frenar() {
+		velocidad=velocidad-15;
+		if (velocidad<0);{
+			velocidad=0;
+		}
+	}
+	public int getVelocidad() {
+		
+		return velocidad;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public int getPotencia() {
+		return potencia;
+	}
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+	public int getVelocidadMax() {
+		return velocidadMax;
+	}
+	public void setVelocidadMax(int velocidadMax) {
+		this.velocidadMax = velocidadMax;
+	}
+	
+	
+
+}
---
+++ Objetos/src/objetos/PruebaCoche.java
@@ -0,0 +1,41 @@
+package objetos;
+
+import java.util.Scanner;
+
+public class PruebaCoche {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		/*Coche coche1 = new Coche();
+		System.out.println("Marca: ");
+		coche1.marca=entrada.nextLine();
+		
+		System.out.println("Modelo: ");
+		coche1.modelo=entrada.nextLine();
+		
+		System.out.println("Potencia: ");
+		coche1.potencia=entrada.nextInt();*/
+		
+		Coche coche2= new Coche();
+		coche2.setMarca("Ford");
+		coche2.setModelo("Mondeo");
+		coche2.setPotencia(120);
+		coche2.setVelocidadMax(40);
+		coche2.acelerar();
+		coche2.acelerar();
+		coche2.acelerar();
+		coche2.acelerar();
+		
+		System.out.println("El coche va a "+coche2.getVelocidad());
+		
+		coche2.frenar();
+		System.out.println("El coche va a "+coche2.getVelocidad());
+		
+		Coche c3=new Coche("Seat", "Leon",105,170);
+		
+		
+
+	}
+
+}
